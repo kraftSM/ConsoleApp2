94d5f9924c5a4dfefd67ef1ef4d6b6cbcbee6c65
kraftSM <kraftsm@yandex.ru>
2022-12-09 10:43:50 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string YurName,MyName, InpStr;
+            
+            string sName =" !! ";
+            bool bHavePet = false;
+            byte bAge =0;
+            double dFootSize =0.0;
+
+            ShowAll(sName, bHavePet, bAge, dFootSize);
+            MyName = "Serge";
+            YurName = "Незнакомец";
+            Console.WriteLine("Здравствуйте  {0}, мое имя {1}. \n А как ваше?", YurName,MyName);
+            InpStr = Console.ReadLine();
+            YouSay(InpStr);
+
+            SayOh(YurName, InpStr);
+            //Console.WriteLine("You say "+ InpStr);
+            Console.WriteLine("\u0023");
+            Console.ReadKey();
+        }
+
+        static void ShowAll(string strVal, bool boolVal, byte byteVal, double doublVal)
+        {
+            const string strName = "Name";
+            const string boolName = "Have a Pet";
+            const string byteName = "Age";
+            const string dublName = "Foot size";
+
+            Console.WriteLine("ShowAll: {0} = {1}", strName, strVal);
+            Console.WriteLine("ShowAll: {0} = {1}", boolName, boolVal);
+            Console.WriteLine("ShowAll: {0} = {1}", byteName, byteVal);
+            Console.WriteLine("ShowAll: {0} = {1}", dublName, doublVal);
+            // Console.WriteLine("Мне сообщили, что Вам {0}", age);
+        }
+        static void Greet(string name, string age)
+        {
+            Console.WriteLine("Здравствуйте, {0}", name);
+            Console.WriteLine("Мне сообщили, что Вам {0}", age);
+        }
+        static void SayOh(string name, string voice)
+        {
+            Console.WriteLine("Еще раз здравствуйте, {0}", name);
+            Console.WriteLine("{0}, вы правда сказали {1}?", name, voice);
+        }
+        static void YouSay(string voice)
+        {
+            Console.WriteLine("Вы сказали {0}", voice);
+        }
+    }
+}
